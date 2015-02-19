02f06d814648e1e676d2abaf30758e4c11e9fb67
Sada <sada.narayanappa@gmail.com>
2015-02-19 14:46:43 -0700

    Commiting by Sada

---
+++ scripts/users.sql
@@ -0,0 +1,21 @@
+--
+-- RUN as
+-- psql -h localhost -U postgres  -d SCHASDB -f tables.sql
+--
+DROP TABLE users;
+
+CREATE TABLE users
+(
+    userid serial		PRIMARY KEY     ,
+    fname           VARCHAR(32)     ,
+    lname           VARCHAR(32)     ,
+    cname           VARCHAR(32)     ,
+    address         VARCHAR(128)    ,
+    cellphone       VARCHAR(16)     ,
+    mobileid        VARCHAR(32)     ,
+    homephone       VARCHAR(16)     ,
+    passwd          VARCHAR(128)    ,
+    home_location   VARCHAR(128)
+);
+
+

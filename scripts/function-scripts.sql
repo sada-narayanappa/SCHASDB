412b16390202f613f95e9288cd5c86c61086e5f1
Wade Meyers <meyersw3476@my.uwstout.edu>
2016-04-04 14:50:28 -0500

    Added function angleindegrees
    Added function to get the angle between points in degrees.

--- scripts/function-scripts.sql
+++ scripts/function-scripts.sql
@@ -165,3 +165,33 @@ WHERE
 	loc1.mobile_id = loc3.mobile_id AND
 	loc1.id = $1.id
 $func$ LANGUAGE SQL STABLE;
+--------------------------------------------------------------------------
+--8
+CREATE OR REPLACE FUNCTION angleindegrees(loc)
+	RETURNS float8 AS 
+$func$
+WITH rows AS (
+	SELECT 
+		mobile_id,
+		id,
+		lat,
+		lon,
+		ROW_NUMBER() OVER (ORDER BY mobile_id, id DESC) AS rn 
+	FROM 
+		loc 
+	WHERE 
+		(record_type <> 'active' OR record_type IS NULL)
+) 
+SELECT  
+	(180/pi())*acos(CASE WHEN ((69.1*(loc2.lat-loc3.lat)*69.1*(loc2.lat-loc3.lat)+(69.1*(loc2.lon - loc3.lon)*cos(loc3.lat/57.3))*(69.1*(loc2.lon - loc3.lon)*cos(loc3.lat/57.3)))-(69.1*(loc1.lat-loc3.lat)*69.1*(loc1.lat-loc3.lat)+(69.1*(loc1.lon - loc3.lon)*cos(loc3.lat/57.3))*(69.1*(loc1.lon - loc3.lon)*cos(loc3.lat/57.3)))-(69.1*(loc1.lat-loc2.lat)*69.1*(loc1.lat-loc2.lat)+(69.1*(loc1.lon - loc2.lon)*cos(loc2.lat/57.3))*(69.1*(loc1.lon - loc2.lon)*cos(loc2.lat/57.3))))/nullif(-2*sqrt(69.1*(loc1.lat-loc3.lat)*69.1*(loc1.lat-loc3.lat)+(69.1*(loc1.lon - loc3.lon)*cos(loc3.lat/57.3))*(69.1*(loc1.lon - loc3.lon)*cos(loc3.lat/57.3)))*sqrt(69.1*(loc1.lat-loc2.lat)*69.1*(loc1.lat-loc2.lat)+(69.1*(loc1.lon - loc2.lon)*cos(loc2.lat/57.3))*(69.1*(loc1.lon - loc2.lon)*cos(loc2.lat/57.3))),0) < -1 THEN -1 WHEN ((69.1*(loc2.lat-loc3.lat)*69.1*(loc2.lat-loc3.lat)+(69.1*(loc2.lon - loc3.lon)*cos(loc3.lat/57.3))*(69.1*(loc2.lon - loc3.lon)*cos(loc3.lat/57.3)))-(69.1*(loc1.lat-loc3.lat)*69.1*(loc1.lat-loc3.lat)+(69.1*(loc1.lon - loc3.lon)*cos(loc3.lat/57.3))*(69.1*(loc1.lon - loc3.lon)*cos(loc3.lat/57.3)))-(69.1*(loc1.lat-loc2.lat)*69.1*(loc1.lat-loc2.lat)+(69.1*(loc1.lon - loc2.lon)*cos(loc2.lat/57.3))*(69.1*(loc1.lon - loc2.lon)*cos(loc2.lat/57.3))))/nullif(-2*sqrt(69.1*(loc1.lat-loc3.lat)*69.1*(loc1.lat-loc3.lat)+(69.1*(loc1.lon - loc3.lon)*cos(loc3.lat/57.3))*(69.1*(loc1.lon - loc3.lon)*cos(loc3.lat/57.3)))*sqrt(69.1*(loc1.lat-loc2.lat)*69.1*(loc1.lat-loc2.lat)+(69.1*(loc1.lon - loc2.lon)*cos(loc2.lat/57.3))*(69.1*(loc1.lon - loc2.lon)*cos(loc2.lat/57.3))),0) > 1 THEN 1 ELSE ((69.1*(loc2.lat-loc3.lat)*69.1*(loc2.lat-loc3.lat)+(69.1*(loc2.lon - loc3.lon)*cos(loc3.lat/57.3))*(69.1*(loc2.lon - loc3.lon)*cos(loc3.lat/57.3)))-(69.1*(loc1.lat-loc3.lat)*69.1*(loc1.lat-loc3.lat)+(69.1*(loc1.lon - loc3.lon)*cos(loc3.lat/57.3))*(69.1*(loc1.lon - loc3.lon)*cos(loc3.lat/57.3)))-(69.1*(loc1.lat-loc2.lat)*69.1*(loc1.lat-loc2.lat)+(69.1*(loc1.lon - loc2.lon)*cos(loc2.lat/57.3))*(69.1*(loc1.lon - loc2.lon)*cos(loc2.lat/57.3))))/nullif(-2*sqrt(69.1*(loc1.lat-loc3.lat)*69.1*(loc1.lat-loc3.lat)+(69.1*(loc1.lon - loc3.lon)*cos(loc3.lat/57.3))*(69.1*(loc1.lon - loc3.lon)*cos(loc3.lat/57.3)))*sqrt(69.1*(loc1.lat-loc2.lat)*69.1*(loc1.lat-loc2.lat)+(69.1*(loc1.lon - loc2.lon)*cos(loc2.lat/57.3))*(69.1*(loc1.lon - loc2.lon)*cos(loc2.lat/57.3))),0) END) 
+
+FROM rows loc1 
+JOIN rows loc2 
+ON loc1.rn = loc2.rn + 1 
+JOIN rows loc3 
+ON  loc1.rn = loc3.rn - 1 
+WHERE 
+	loc1.mobile_id = loc2.mobile_id AND 
+	loc1.mobile_id = loc3.mobile_id AND
+	loc1.id = $1.id
+$func$ LANGUAGE SQL STABLE;

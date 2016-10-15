d634d5eeeeb57ee571788b299cf812e73f05b4e2
sada-narayanappa <sada.narayanappa@gmail.com>
2016-10-14 23:09:05 -0600

    Commiting by Sada

---
+++ ec2/sshm.sh
@@ -0,0 +1,1 @@
+ssh -i "schas.pem" centos@ec2-52-37-253-191.us-west-2.compute.amazonaws.com
--- ec2/sshs.sh
+++ ec2/sshs.sh
@@ -1,3 +1,4 @@
-ssh -i "student.pem" centos@ec2-52-25-152-209.us-west-2.compute.amazonaws.com
+ssh -i "student.pem" centos@ec2-52-89-247-43.us-west-2.compute.amazonaws.com
+#ssh -i "student.pem" centos@ec2-52-25-152-209.us-west-2.compute.amazonaws.com
 #ssh -i "student.pem" 52.25.152.209
 

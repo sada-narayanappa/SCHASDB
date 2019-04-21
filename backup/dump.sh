df587cd6d2ba3562d317b5d93ca423d643f4598b
sada-narayanappa <sada.narayanappa@gmail.com>
2019-04-21 22:39:20 +0000

    Commiting by Sada

--- backup/dump.sh
+++ backup/dump.sh
@@ -1,7 +1,8 @@
 export LATEST=SCHASDB.gz
 export BACKUPFILE=SCHASDB-`date +%Y-%m-%d_%H-%M-%S`.gz
 echo "Backing up to " $BACKUPFILE
+#pg_dump -h geospaces.org -U postgres SCHASDB | gzip > $BACKUPFILE
+pg_dump -h 127.0.0.1  -U postgres SCHASDB | gzip > $BACKUPFILE
 rm -i $LATEST
-pg_dump -h geospaces.org -U postgres SCHASDB | gzip > $BACKUPFILE
-cp $BACKUPFILE $LATEST
+ln -s $BACKUPFILE $LATEST
 
